83bce7ade05c20ccd72d51c6025516ed55cf9951
S-Bou <bouyo30@gmail.com>
2023-07-01 10:23:17 +0200

    202307011023

--- HerenciaPOO.py
+++ HerenciaPOO.py
@@ -19,10 +19,47 @@ class Vehiculos():
 
 	def Estado(self):
 		print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn marcha: ",
-			self.enmarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena)
+			self.enmarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena, "\n")
+
+class Furgoneta(Vehiculos):
+
+	def Carga(self, cargar):
+		self.cargado = cargar
+		if(self.cargado):
+			return "La furgoneta está cargada"
+		else:
+			return "La furgoneta no está cargada"
 
 class Moto(Vehiculos):
+	hcaballito = ""
+
+	def Caballito(self):
+		self.hcaballito = "Haciendo caballito"
+
+	def Estado(self):
+		print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn marcha: ",
+			self.enmarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena,
+			"\n", self.hcaballito)
+
+class VElectrico():
+
+	def __init__(self):
+		self.autonomia = 100
+
+	def CargarEnergia(self):
+		self.cargando = True
+
+class BicicletaElectrica(Vehiculos, VElectrico):
 	pass
 
+miBici = BicicletaElectrica("Orbea", "HBO")
+miBici.Estado()
+
 miMoto = Moto("Yamaha", "XVS")
+miMoto.Caballito()
 miMoto.Estado()
+
+miFurgoneta = Furgoneta("Mercedes", "Vito")
+miFurgoneta.Arrancar()
+miFurgoneta.Estado()
+print(miFurgoneta.Carga(True))
